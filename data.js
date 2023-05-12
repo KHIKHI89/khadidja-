67f41ec56eb2cffe8ab8130bcbbce7dcfcf57346
Khadidja <111190473+KHIKHI89@users.noreply.github.com>
2023-05-13 00:34:45 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,143 @@
+export const data =
+[
+    {
+        "_id": "61823feacbdc98001104e059",
+        "idDossier": {
+            "$id": "5f3b82dfd51bbd00243b211e"
+        },
+        "title": "Tran Cong, Ly / Tran Cong, Ly / 77600 Bussy-Saint-Georges",
+        "status": "invoiced",
+        "startDate": "2021-11-17",
+        "endDate": "2021-11-17",
+        "address": "1 Allée Maryse Bastié 77600 Bussy-Saint-Georges FR",
+        "metiers": "professionRDF",
+        "internalGlobalAmount": 1,
+        "prestationComment": null
+    },
+    {
+        "_id": "60746a652642f8001284867b",
+        "idDossier": {
+            "$id": "603e3da289d91d00118423e9"
+        },
+        "title": "Nicolas Pascaud / Nicolas Pascaud / 77400 Saint-Thibault-Des-Vignes",
+        "status": "invoiced",
+        "startDate": "2022-4-20",
+        "endDate": "2022-4-20",
+        "address": "9 Villa Paul Cézanne 77400 Saint-Thibault-Des-Vignes FR",
+        "metiers": "professionRDF",
+        "internalGlobalAmount": 2,
+        "prestationComment": null
+    },
+    {
+        "_id": "615718f1f7d06d0011af45a8",
+        "idDossier": {
+            "$id": "6090f10210f49f0012feeecf"
+        },
+        "title": "Isabel Do Couto / Isabel Do Couto / 77100 Meaux",
+        "status": "invoiced",
+        "startDate": "2021-11-2",
+        "endDate": "2021-11-5",
+        "address": "4 Square Jean Aicard 77100 Meaux FR",
+        "metiers": "professionRenovation",
+        "internalGlobalAmount": 4,
+        "prestationComment": null
+    },
+    {
+        "_id": "60d05839764b860011eead63",
+        "idDossier": {
+            "$id": "60ae496945a7d700116889d5"
+        },
+        "title": "2e inter / Naji Fouad / 77350 Le Mée-Sur-Seine",
+        "status": "invoiced",
+        "startDate": "2022-2-18",
+        "endDate": "2022-2-18",
+        "address": "39 Rue Léon Germain 77350 Le Mée-Sur-Seine FR",
+        "metiers": "professionRDF",
+        "internalGlobalAmount": 4,
+        "prestationComment": null
+    },
+    {
+        "_id": "61f7b41eb1265a0012dd8d0a",
+        "idDossier": {
+            "$id": "60be1cfbfd98be0012028635"
+        },
+        "title": "Maria Correia / Maria Correia / 77130 Varennes-Sur-Seine",
+        "status": "invoiced",
+        "startDate": "2022-2-2",
+        "endDate": "2022-2-2",
+        "address": "85 Grande Rue 77130 Varennes-Sur-Seine FR",
+        "metiers": "professionRDF",
+        "internalGlobalAmount": 0,
+        "prestationComment": null
+    },
+    {
+        "_id": "619e04f1ef14af0011a30890",
+        "idDossier": {
+            "$id": "613f543178c95600144132b6"
+        },
+        "title": "Clement Holin / Clement Holin / 77860 Quincy-Voisins",
+        "status": "terminated",
+        "startDate": "2022-1-18",
+        "endDate": "2022-1-18",
+        "address": "5 Rue Thiers 77860 Quincy-Voisins FR",
+        "metiers": "professionRDF",
+        "internalGlobalAmount": 5,
+        "prestationComment": null
+    },
+    {
+        "_id": "61ddbc396086ee0011c24d2e",
+        "idDossier": {
+            "$id": "613f2e8978c956001440c831"
+        },
+        "title": "Pointeau / Syndic du 25 rue de france / 77300 Fontainebleau",
+        "status": "invoiced",
+        "startDate": "2022-1-21",
+        "endDate": "2022-1-21",
+        "address": "25 Rue de France 77300 Fontainebleau FR",
+        "metiers": "professionRDF",
+        "internalGlobalAmount": 654,
+        "prestationComment": null
+    },
+    {
+        "_id": "6182734f4ae92c00112a2b89",
+        "idDossier": {
+            "$id": "614898675d03310011cbc653"
+        },
+        "title": "Sayd Abdelkader / 77500 Chelles",
+        "status": "invoiced",
+        "startDate": "2021-11-4",
+        "endDate": "2021-11-4",
+        "address": "43 Boulevard Chilperic 77500 Chelles FR",
+        "metiers": "professionRDF",
+        "internalGlobalAmount": 432,
+        "prestationComment": null
+    },
+    {
+        "_id": "6170ff71570b0800112e94f8",
+        "idDossier": {
+            "$id": "615f197570f50800125e534b"
+        },
+        "title": "Liveli BY Sodexo / Liveli BY Sodexo / 77600 Bussy-Saint-Georges",
+        "status": "invoiced",
+        "startDate": "2021-11-12",
+        "endDate": "2021-11-12",
+        "address": "3 Rue Aristide Maillol 77600 Bussy-Saint-Georges FR",
+        "metiers": "professionRDF",
+        "internalGlobalAmount": 553,
+        "prestationComment": null
+    },
+    {
+        "_id": "616e719ba2dec7001193cd18",
+        "idDossier": {
+            "$id": "616944d7bf06020012a58309"
+        },
+        "title": "Sarah Patard / 77230 Moussy-le-Neuf",
+        "status": "invoiced",
+        "startDate": "2021-11-3",
+        "endDate": "2021-11-3",
+        "address": "25 Rue Cambacérès 77230 Moussy-le-Neuf FR",
+        "metiers": "professionRDF",
+        "internalGlobalAmount": 643,
+        "prestationComment": null
+    }
+]
